bda4c3f5038e721a9c48a1b091d3b128ddf5402f
stevenz33 <steven.t.zhou@cn.pwc.com>
2021-10-26 16:19:21 +0800

    sync

---
+++ algorithm/quickPow/quickPow.py
@@ -0,0 +1,12 @@
+mod = 10**9 +7
+def quickPow(x,y):
+    ret =1
+    cur = x 
+    while y >0:
+        if y & 1:
+            ret = ret * cur % mod
+        cur = cur *cur % mod
+        y = y //2
+    return ret
+
+print(quickPow(2, 100000002))
