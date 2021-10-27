16c2d3d0488feb284f61e80e7924b4c0d3b018d3
stevenz33 <steven.t.zhou@cn.pwc.com>
2021-10-27 17:06:00 +0800

    sync

--- questions/0000/t12.py
+++ questions/0000/t12.py
@@ -9,14 +9,11 @@ def getNumber(index):
     while cnt < index:
         res = heapq.heappop(hp)
         cnt +=1
-        #print("aa : ", res)
         for i in primes:
             t2 = res * i
-            #print(t2)
             if t2 not in dic:
                 dic[t2] =1
                 heapq.heappush(hp, t2)
-        #print(hp)
         mx = max(mx,len(hp))
     return (res,mx)
 
