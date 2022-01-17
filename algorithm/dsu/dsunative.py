06a59c5aa1e716a196a13231977367a340145e7c
stevenz33 <steven.t.zhou@cn.pwc.com>
2022-01-17 15:13:51 +0800

    sync

---
+++ algorithm/dsu/dsunative.py
@@ -0,0 +1,14 @@
+#this will record "father order" which tarjan needs 
+# Only this way will works for ../tree/lca/tarjan.py
+class DSU:
+    def __init__(self,N):
+        self.p  = list(range(N))
+        self.rank = [1]*N
+    
+    def find(self,x):
+        if self.p[x] != x:
+            self.p[x] =self.find(self.p[x])
+        return self.p[x]
+    
+    def union(self,x,y):
+        self.p[y]= x
