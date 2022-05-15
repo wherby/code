29a3735ab2a3324047b451ef321c8e454c961b06
wherby <187225577@qq.com>
2022-05-15 13:09:14 +0800

    sync

---
+++ algorithm/spanMerge/SpanMerg.py
@@ -0,0 +1,31 @@
+# https://leetcode.cn/contest/weekly-contest-293/problems/count-integers-in-intervals/
+from sortedcontainers import SortedList
+from bisect import bisect_right,bisect_left
+class Span():
+    def __init__(self):
+        self.span =SortedList([(-1,-1),(10**10,10**10)])
+    
+    def add(self,left,right):
+        leftIdx = bisect_left(self.span,(left,0))
+        leftIdx -=1
+        if self.span[leftIdx][1] <left-1:
+            leftIdx +=1
+        remove=[]
+        endIdx = leftIdx
+        while  endIdx < len(self.span) and self.span[endIdx][0] <=right +1:
+            remove.append(self.span[endIdx])
+            endIdx +=1
+        newLeft,newRight = left,right
+        for item in remove:
+            newLeft = min(newLeft,item[0])
+            newRight = max(newRight,item[1])
+            self.span.remove(item)
+        self.span.add((newLeft,newRight))
+        
+
+re =Span()
+re.add(2,3)
+re.add(5,8)
+re.add(7,10)
+print(re.span)
+        
