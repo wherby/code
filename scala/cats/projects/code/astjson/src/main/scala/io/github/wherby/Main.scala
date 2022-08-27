a1957d8fd196489f23334cfb1851080d449f95b0
wherby <187225577@qq.com>
2022-08-27 17:44:26 +0800

    add printablesyntax

--- scala/cats/projects/code/astjson/src/main/scala/io/github/wherby/Main.scala
+++ scala/cats/projects/code/astjson/src/main/scala/io/github/wherby/Main.scala
@@ -4,17 +4,19 @@ import cats.implicits._
 import JsonWriterInstances._
 import JsonSyntax._
 import io.github.wherby.models.Cat
-import io.github.wherby.printable.Printable
 import io.github.wherby.printable.PrintableInstances._
+import io.github.wherby.printable.PrintableSyntax._
 
 object Main extends App {
   val ret = Person("Dave", "dave@example.com").toJson
   println(ret)
   println("Hello " |+| "Cats!")
   println(Json.toJson(Option("A string")))
-  val cat = Cat("Garfield", 41, "ginger and black")
-// cat: Cat = Cat("Garfield", 41, "ginger and black")
+//   val cat = Cat("Garfield", 41, "ginger and black")
+// // cat: Cat = Cat("Garfield", 41, "ginger and black")
 
-  Printable.print(cat)
-// Garfield is a 41 year-old ginger and black cat.
+//   Printable.print(cat)
+// // Garfield is a 41 year-old ginger and black cat.
+
+  Cat("Garfield", 41, "ginger and black").print
 }
---
+++ scala/cats/projects/code/astjson/src/main/scala/io/github/wherby/printable/PrintableSyntax.scala
@@ -0,0 +1,10 @@
+package io.github.wherby.printable
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit p: Printable[A]): String =
+      p.format(value)
+
+    def print(implicit p: Printable[A]): Unit =
+      println(format(p))
+  }
+}
