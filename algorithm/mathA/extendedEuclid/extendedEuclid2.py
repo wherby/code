51bea4a5aa999a10e5d8c06fbc54739f5e43aa03
wherby <187225577@qq.com>
2023-04-16 22:24:14 +0800

    sb

---
+++ algorithm/mathA/extendedEuclid/extendedEuclid2.py
@@ -0,0 +1,32 @@
+# https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/
+
+# https://codingcompetitions.withgoogle.com/codejam/round/0000000000c9607c/0000000000cad13d
+# https://www.youtube.com/watch?v=JTC3LVy2uTI&ab_channel=NealWu
+# algorithm/mathA/extendedEuclid/pdf/extendedEuclid.pdf
+
+
+ 
+def modInverse(a, m):
+    g = m 
+    r = a 
+    x,y = 0,1
+    
+    while r !=0:
+        q = g //r 
+        g = g %r 
+        g,r = r,g 
+        x -= q *y 
+        x,y = y,x
+    return x if x>0 else x+m
+ 
+ 
+# Driver code
+if __name__ == "__main__":
+    A = 3
+    M = 11
+ 
+    # Function call
+    print("Modular multiplicative inverse is",
+          modInverse(A, M))
+ 
+# This code is contributed by Nikita tiwari.
