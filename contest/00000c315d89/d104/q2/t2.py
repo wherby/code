adb83c0b855d74fa3d0459b3f088b3701bfd3845
wherby <187225577@qq.com>
2023-05-13 22:18:12 +0800

    sync

---
+++ contest/00000c315d89/d104/q2/t2.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple, Optional
+
+from collections import defaultdict,deque
+from functools import cache
+import heapq
+from sortedcontainers import SortedDict,SortedList
+class Solution:
+    pass
+
+
+
+
+
+re =Solution()
+print(re)
