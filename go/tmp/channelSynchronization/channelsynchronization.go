eaa54b876092093117632858a1ce76c864d6cd47
wherby <187225577@qq.com>
2025-05-20 17:12:13 +0800

    sync

---
+++ go/tmp/channelSynchronization/channelsynchronization.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Println("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 2)
+	go worker(done)
+
+	<-done
+}
